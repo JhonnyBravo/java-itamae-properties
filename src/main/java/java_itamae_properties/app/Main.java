45634e3df57b549c94849c407d1fb16656582ae7
JhonnyBravo <sanfranceshika5@gmail.com>
2020-08-29 16:24:46 +0900

    modified: src/main/java/java_itamae_properties/app/Main.java
    ラムダ式を導入。

--- src/main/java/java_itamae_properties/app/Main.java
+++ src/main/java/java_itamae_properties/app/Main.java
@@ -1,6 +1,8 @@
 package java_itamae_properties.app;
 
 import java.io.File;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,29 +19,36 @@ import java_itamae_properties.domain.service.properties.PropertiesServiceImpl;
 public class Main {
     /**
      * @param args
-     *             <ul>
-     *             <li>--path, -p &lt;path&gt;: 操作対象とするファイルのパスを指定する。</li>
-     *             <li>--encoding, -e &lt;encoding&gt;: 文字エンコーディングを指定する。</li>
-     *             <li>--key, -k &lt;key&gt;: 操作対象とするプロパティのキー名を指定する。</li>
-     *             <li>--value, -v &lt;value&gt;: プロパティへ書込む値を指定する。</li>
-     *             <li>--get-property, -g: ファイルからプロパティの値を取得して標準出力へ出力する。</li>
-     *             <li>--create-property, -c: プロパティを新規登録する。</li>
-     *             <li>--update-property, -u: プロパティの値を更新する。</li>
-     *             <li>--delete-property, -d: プロパティを削除する。</li>
-     *             </ul>
+     *            <ul>
+     *            <li>--path, -p &lt;path&gt;: 操作対象とするファイルのパスを指定する。</li>
+     *            <li>--encoding, -e &lt;encoding&gt;: 文字エンコーディングを指定する。</li>
+     *            <li>--key, -k &lt;key&gt;: 操作対象とするプロパティのキー名を指定する。</li>
+     *            <li>--value, -v &lt;value&gt;: プロパティへ書込む値を指定する。</li>
+     *            <li>--get-property, -g: ファイルからプロパティの値を取得して標準出力へ出力する。</li>
+     *            <li>--create-property, -c: プロパティを新規登録する。</li>
+     *            <li>--update-property, -u: プロパティの値を更新する。</li>
+     *            <li>--delete-property, -d: プロパティを削除する。</li>
+     *            </ul>
      */
     public static void main(String[] args) {
         final LongOpt[] longopts = new LongOpt[8];
         longopts[0] = new LongOpt("path", LongOpt.REQUIRED_ARGUMENT, null, 'p');
-        longopts[1] = new LongOpt("encoding", LongOpt.REQUIRED_ARGUMENT, null, 'e');
+        longopts[1] = new LongOpt("encoding", LongOpt.REQUIRED_ARGUMENT, null,
+                'e');
         longopts[2] = new LongOpt("key", LongOpt.REQUIRED_ARGUMENT, null, 'k');
-        longopts[3] = new LongOpt("value", LongOpt.REQUIRED_ARGUMENT, null, 'v');
-        longopts[4] = new LongOpt("get-property", LongOpt.NO_ARGUMENT, null, 'g');
-        longopts[5] = new LongOpt("create-property", LongOpt.NO_ARGUMENT, null, 'c');
-        longopts[6] = new LongOpt("update-property", LongOpt.NO_ARGUMENT, null, 'u');
-        longopts[7] = new LongOpt("delete-property", LongOpt.NO_ARGUMENT, null, 'd');
+        longopts[3] = new LongOpt("value", LongOpt.REQUIRED_ARGUMENT, null,
+                'v');
+        longopts[4] = new LongOpt("get-property", LongOpt.NO_ARGUMENT, null,
+                'g');
+        longopts[5] = new LongOpt("create-property", LongOpt.NO_ARGUMENT, null,
+                'c');
+        longopts[6] = new LongOpt("update-property", LongOpt.NO_ARGUMENT, null,
+                'u');
+        longopts[7] = new LongOpt("delete-property", LongOpt.NO_ARGUMENT, null,
+                'd');
 
-        final Getopt options = new Getopt("Main", args, "p:e:k:v:gcud", longopts);
+        final Getopt options = new Getopt("Main", args, "p:e:k:v:gcud",
+                longopts);
         final Logger logger = LoggerFactory.getLogger(Main.class);
 
         final ContentsAttribute attr = new ContentsAttribute();
@@ -55,85 +64,149 @@ public class Main {
 
         while ((c = options.getopt()) != -1) {
             switch (c) {
-            case 'p':
-                attr.setPath(options.getOptarg());
-                break;
-            case 'e':
-                attr.setEncoding(options.getOptarg());
-                break;
-            case 'k':
-                key = options.getOptarg();
-                break;
-            case 'v':
-                value = options.getOptarg();
-                break;
-            case 'g':
-                getFlag = 1;
-                break;
-            case 'c':
-                createFlag = 1;
-                break;
-            case 'u':
-                updateFlag = 1;
-                break;
-            case 'd':
-                deleteFlag = 1;
-                break;
+                case 'p' :
+                    attr.setPath(options.getOptarg());
+                    break;
+                case 'e' :
+                    attr.setEncoding(options.getOptarg());
+                    break;
+                case 'k' :
+                    key = options.getOptarg();
+                    break;
+                case 'v' :
+                    value = options.getOptarg();
+                    break;
+                case 'g' :
+                    getFlag = 1;
+                    break;
+                case 'c' :
+                    createFlag = 1;
+                    break;
+                case 'u' :
+                    updateFlag = 1;
+                    break;
+                case 'd' :
+                    deleteFlag = 1;
+                    break;
             }
         }
 
-        if (getFlag == 1 || createFlag == 1 || updateFlag == 1 || deleteFlag == 1) {
+        int status = 0;
+
+        if (getFlag == 1 || createFlag == 1 || updateFlag == 1
+                || deleteFlag == 1) {
             if (attr.getPath() == null) {
                 logger.warn("path オプションを指定してください。");
-                System.exit(1);
+                status = 1;
             }
 
             if (key == null) {
                 logger.warn("key オプションを指定してください。");
-                System.exit(1);
+                status = 1;
             }
         }
 
         if (createFlag == 1 || updateFlag == 1) {
             if (value == null) {
                 logger.warn("value オプションを指定してください。");
-                System.exit(1);
+                status = 1;
             }
         }
 
         final PropertiesService ps = new PropertiesServiceImpl(attr);
-        boolean status = false;
+
+        Function<String, Integer> getProperty = keyName -> {
+            try {
+                String keyValue = ps.getProperty(keyName);
+
+                if (keyValue != null && !keyValue.isEmpty()) {
+                    System.out.println(keyValue);
+                    return 2;
+                } else {
+                    return 0;
+                }
+            } catch (Exception e) {
+                logger.warn(e.toString());
+                return 1;
+            }
+        };
+
+        BiFunction<String, String, Integer> createProperty = (keyName,
+                keyValue) -> {
+            try {
+                boolean result = ps.createProperty(keyName, keyValue);
+
+                if (result) {
+                    return 2;
+                } else {
+                    return 0;
+                }
+            } catch (Exception e) {
+                logger.warn(e.toString());
+                return 1;
+            }
+        };
+
+        BiFunction<String, String, Integer> updateProperty = (keyName,
+                keyValue) -> {
+            try {
+                boolean result = ps.updateProperty(keyName, keyValue);
+
+                if (result) {
+                    return 2;
+                } else {
+                    return 0;
+                }
+            } catch (Exception e) {
+                logger.warn(e.toString());
+                return 1;
+            }
+        };
+
+        Function<String, Integer> deleteProperty = keyName -> {
+            try {
+                boolean result = ps.deleteProperty(keyName);
+
+                if (result) {
+                    return 2;
+                } else {
+                    return 0;
+                }
+            } catch (Exception e) {
+                logger.warn(e.toString());
+                return 1;
+            }
+        };
 
         try {
             if (getFlag == 1) {
-                value = ps.getProperty(key);
-
-                if (value != null && !value.isEmpty()) {
-                    System.out.println(value);
-                    status = true;
+                if (status != 1) {
+                    status = getProperty.apply(key);
                 }
             } else if (createFlag == 1) {
-                final File file = new File(attr.getPath());
+                if (status != 1) {
+                    final File file = new File(attr.getPath());
 
-                if (!file.isFile()) {
-                    file.createNewFile();
+                    if (!file.isFile()) {
+                        file.createNewFile();
+                    }
+
+                    status = createProperty.apply(key, value);
                 }
-
-                status = ps.createProperty(key, value);
             } else if (updateFlag == 1) {
-                status = ps.updateProperty(key, value);
+                if (status != 1) {
+                    status = updateProperty.apply(key, value);
+                }
             } else if (deleteFlag == 1) {
-                status = ps.deleteProperty(key);
-            }
-
-            if (status) {
-                System.exit(2);
-            } else {
-                System.exit(0);
+                if (status != 1) {
+                    status = deleteProperty.apply(key);
+                }
             }
         } catch (final Exception e) {
             logger.warn(e.toString());
-            System.exit(1);
+            status = 1;
         }
+
+        System.exit(status);
     }
 }
