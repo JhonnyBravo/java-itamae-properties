44cdf3320a2fe41093d5514533b9b6a7760b5530
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-30 17:34:33 +0900

    modified: src/main/java/java_itamae_properties/domain/repository/properties/PropertiesRepositoryImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae_properties/domain/repository/properties/PropertiesRepositoryImpl.java
+++ src/main/java/java_itamae_properties/domain/repository/properties/PropertiesRepositoryImpl.java
@@ -8,43 +8,44 @@ import java.util.Properties;
 
 public class PropertiesRepositoryImpl implements PropertiesRepository {
 
-    @Override
-    public Map<String, String> getProperties(Reader reader) throws Exception {
-        final Properties properties = new Properties();
-        properties.load(reader);
+  @Override
+  public Map<String, String> getProperties(Reader reader) throws Exception {
+    final Properties properties = new Properties();
+    properties.load(reader);
 
-        final Map<String, String> map = new HashMap<String, String>();
+    final Map<String, String> map = new HashMap<>();
 
-        for (final Map.Entry<Object, Object> entry : properties.entrySet()) {
-            map.put(entry.getKey().toString(), entry.getValue().toString());
-        }
-
-        return map;
+    for (final Map.Entry<Object, Object> entry : properties.entrySet()) {
+      map.put(entry.getKey().toString(), entry.getValue().toString());
     }
 
-    @Override
-    public boolean setProperties(Writer writer, Map<String, String> map, String comment) throws Exception {
-        boolean status = false;
-        final Properties properties = new Properties();
+    return map;
+  }
 
-        for (final Map.Entry<String, String> entry : map.entrySet()) {
-            properties.setProperty(entry.getKey(), entry.getValue());
-        }
+  @Override
+  public boolean updateProperties(Writer writer, Map<String, String> map, String comment)
+      throws Exception {
+    boolean status = false;
+    final Properties properties = new Properties();
 
-        properties.store(writer, comment);
-        status = true;
-
-        return status;
+    for (final Map.Entry<String, String> entry : map.entrySet()) {
+      properties.setProperty(entry.getKey(), entry.getValue());
     }
 
-    @Override
-    public boolean deleteProperties(Writer writer, String comment) throws Exception {
-        boolean status = false;
+    properties.store(writer, comment);
+    status = true;
 
-        final Properties properties = new Properties();
-        properties.store(writer, comment);
+    return status;
+  }
 
-        status = true;
-        return status;
-    }
+  @Override
+  public boolean deleteProperties(Writer writer, String comment) throws Exception {
+    boolean status = false;
+
+    final Properties properties = new Properties();
+    properties.store(writer, comment);
+
+    status = true;
+    return status;
+  }
 }
