78d49e83943ec60fa80a7fde5f9d35bf19922df5
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-30 17:36:00 +0900

    modified: src/main/java/java_itamae_properties/domain/service/properties/PropertiesServiceImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/java_itamae_properties/domain/service/properties/PropertiesServiceImpl.java
+++ src/main/java/java_itamae_properties/domain/service/properties/PropertiesServiceImpl.java
@@ -5,7 +5,6 @@ import java.io.Reader;
 import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
-
 import java_itamae_contents.domain.model.ContentsAttribute;
 import java_itamae_contents.domain.repository.stream.StreamRepository;
 import java_itamae_contents.domain.repository.stream.StreamRepositoryImpl;
@@ -13,101 +12,103 @@ import java_itamae_properties.domain.repository.properties.PropertiesRepository;
 import java_itamae_properties.domain.repository.properties.PropertiesRepositoryImpl;
 
 public class PropertiesServiceImpl implements PropertiesService {
-    private final ContentsAttribute attr;
-    private final StreamRepository sr;
-    private final PropertiesRepository pr;
+  private final ContentsAttribute attr;
+  private final StreamRepository sr;
+  private final PropertiesRepository pr;
 
-    /**
-     * @param attr 操作対象とするファイルの情報を納めた ContentsAttribute を指定する。
-     */
-    public PropertiesServiceImpl(ContentsAttribute attr) {
-        this.attr = attr;
-        sr = new StreamRepositoryImpl();
-        pr = new PropertiesRepositoryImpl();
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param attr 操作対象とするファイルの情報を納めた {@link ContentsAttribute} を指定する。
+   */
+  public PropertiesServiceImpl(ContentsAttribute attr) {
+    this.attr = attr;
+    sr = new StreamRepositoryImpl();
+    pr = new PropertiesRepositoryImpl();
+  }
+
+  @Override
+  public String getProperty(String key) throws Exception {
+    String value = null;
+
+    try (Reader reader = sr.getReader(attr)) {
+      final Map<String, String> properties = pr.getProperties(reader);
+
+      if (!properties.containsKey(key)) {
+        throw new Exception(key + " が見つかりません。");
+      }
+
+      value = properties.get(key);
     }
 
-    @Override
-    public String getProperty(String key) throws Exception {
-        String value = null;
+    return value;
+  }
 
-        try (Reader reader = sr.getReader(attr)) {
-            final Map<String, String> properties = pr.getProperties(reader);
+  @Override
+  public boolean createProperty(String key, String value) throws Exception {
+    boolean status = false;
+    Map<String, String> properties = new HashMap<>();
 
-            if (!properties.containsKey(key)) {
-                throw new Exception(key + " が見つかりません。");
-            }
-
-            value = properties.get(key);
-        }
-
-        return value;
+    try (Reader reader = sr.getReader(attr)) {
+      properties = pr.getProperties(reader);
     }
 
-    @Override
-    public boolean createProperty(String key, String value) throws Exception {
-        boolean status = false;
-        Map<String, String> properties = new HashMap<String, String>();
-
-        try (Reader reader = sr.getReader(attr)) {
-            properties = pr.getProperties(reader);
-        }
-
-        if (properties.containsKey(key)) {
-            throw new Exception(key + " は登録済みです。");
-        }
-
-        try (Writer writer = sr.getWriter(attr)) {
-            properties.put(key, value);
-            final String fileName = new File(attr.getPath()).getName();
-            status = pr.setProperties(writer, properties, fileName);
-        }
-
-        return status;
+    if (properties.containsKey(key)) {
+      throw new Exception(key + " は登録済みです。");
     }
 
-    @Override
-    public boolean updateProperty(String key, String value) throws Exception {
-        boolean status = false;
-        Map<String, String> properties = new HashMap<String, String>();
-
-        try (Reader reader = sr.getReader(attr)) {
-            properties = pr.getProperties(reader);
-        }
-
-        if (!properties.containsKey(key)) {
-            throw new Exception(key + " が見つかりません。");
-        }
-
-        if (!value.equals(properties.get(key))) {
-            try (Writer writer = sr.getWriter(attr)) {
-                properties.put(key, value);
-                final String fileName = new File(attr.getPath()).getName();
-                status = pr.setProperties(writer, properties, fileName);
-            }
-        }
-
-        return status;
+    try (Writer writer = sr.getWriter(attr)) {
+      properties.put(key, value);
+      final String fileName = new File(attr.getPath()).getName();
+      status = pr.updateProperties(writer, properties, fileName);
     }
 
-    @Override
-    public boolean deleteProperty(String key) throws Exception {
-        boolean status = false;
-        Map<String, String> properties = new HashMap<String, String>();
+    return status;
+  }
 
-        try (Reader reader = sr.getReader(attr)) {
-            properties = pr.getProperties(reader);
-        }
+  @Override
+  public boolean updateProperty(String key, String value) throws Exception {
+    boolean status = false;
+    Map<String, String> properties = new HashMap<>();
 
-        if (!properties.containsKey(key)) {
-            throw new Exception(key + " が見つかりません。");
-        }
-
-        try (Writer writer = sr.getWriter(attr)) {
-            properties.remove(key);
-            final String fileName = new File(attr.getPath()).getName();
-            status = pr.setProperties(writer, properties, fileName);
-        }
-
-        return status;
+    try (Reader reader = sr.getReader(attr)) {
+      properties = pr.getProperties(reader);
     }
+
+    if (!properties.containsKey(key)) {
+      throw new Exception(key + " が見つかりません。");
+    }
+
+    if (!value.equals(properties.get(key))) {
+      try (Writer writer = sr.getWriter(attr)) {
+        properties.put(key, value);
+        final String fileName = new File(attr.getPath()).getName();
+        status = pr.updateProperties(writer, properties, fileName);
+      }
+    }
+
+    return status;
+  }
+
+  @Override
+  public boolean deleteProperty(String key) throws Exception {
+    boolean status = false;
+    Map<String, String> properties = new HashMap<>();
+
+    try (Reader reader = sr.getReader(attr)) {
+      properties = pr.getProperties(reader);
+    }
+
+    if (!properties.containsKey(key)) {
+      throw new Exception(key + " が見つかりません。");
+    }
+
+    try (Writer writer = sr.getWriter(attr)) {
+      properties.remove(key);
+      final String fileName = new File(attr.getPath()).getName();
+      status = pr.updateProperties(writer, properties, fileName);
+    }
+
+    return status;
+  }
 }
