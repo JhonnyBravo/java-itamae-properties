803fe792a370d038c46779874b11fc92a6192155
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-07-03 15:15:55 +0900

    renamed: src/test/java/java_itamae_properties/domain/repository/properties/PropertiesRepositoryTest.java -> TestPropertiesRepository.java}
    CDI Unit の導入に伴い、テストを以下のクラスに分割。
    
    * EmptyFile
    * NotEmptyFile

---
+++ src/test/java/java_itamae_properties/domain/repository/properties/TestPropertiesRepository.java
@@ -0,0 +1,10 @@
+package java_itamae_properties.domain.repository.properties;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({EmptyFile.class, NotEmptyFile.class})
+public class TestPropertiesRepository {
+}
